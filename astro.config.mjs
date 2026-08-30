4cea5727b9e1791d1bf48254edac322e347d85a9
Phil Boden <philip.boden@gmail.com>
2026-08-29 19:37:59 -0700

    Initial first day build of website. pushing to github to activate Github Pages setup for first review.

---
+++ astro.config.mjs
@@ -0,0 +1,74 @@
+// @ts-check
+
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import { defineConfig, fontProviders } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://example.com',
+	integrations: [mdx(), sitemap()],
+	fonts: [
+		{
+			provider: fontProviders.local(),
+			name: 'Atkinson',
+			cssVariable: '--font-atkinson',
+			fallbacks: ['sans-serif'],
+			options: {
+				variants: [
+					{
+						src: ['./src/assets/fonts/atkinson-regular.woff'],
+						weight: 400,
+						style: 'normal',
+						display: 'swap',
+					},
+					{
+						src: ['./src/assets/fonts/atkinson-bold.woff'],
+						weight: 700,
+						style: 'normal',
+						display: 'swap',
+					},
+				],
+			},
+		},
+		// {
+		// 	provider: fontProviders.google(),
+        // 	name: "Inter",
+        // 	cssVariable: "--font-inter",
+        // 	// Optional: specify weights, styles, subsets
+        // 	weights: [400, 500, 700],
+        // 	styles: ["normal", "italic"],
+        // 	subsets: ["latin"],
+		// },
+		// {
+		// 	provider: fontProviders.google(),
+        // 	name: "IBM Plex Serif",
+        // 	cssVariable: "--font-ibm-plex-serif",
+        // 	// Optional: specify weights, styles, subsets
+        // 	weights: [100, 200, 300, 400, 500, 600, 700],
+        // 	styles: ["normal", "italic"],
+        // 	subsets: ["latin"],
+		// },
+		{
+			provider: fontProviders.google(),
+        	name: "Lato",
+        	cssVariable: "--font-lato",
+        	weights: [100, 300, 400, 700, 900],
+        	styles: ["normal", "italic"],
+		},
+		// {
+		// 	provider: fontProviders.google(),
+        // 	name: "Playfair Display",
+        // 	cssVariable: "--font-playfair-display",
+        // 	weights: [400, 500, 600, 700, 800, 900],
+        // 	styles: ["normal", "italic"],
+		// },
+		{
+			provider: fontProviders.google(),
+        	name: "Merriweather",
+        	cssVariable: "--font-merriweather",
+        	weights: [300, 400, 500, 600, 700, 800, 900],
+        	styles: ["normal", "italic"],
+		},
+	],
+});
---
+++ src/consts.ts
@@ -0,0 +1,9 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Veronica Mangrich';
+export const SITE_DESCRIPTION = 'Welcome to my website!';
+
+export const HERO_TITLE = 'Hi, My name is Veronica Mangrich';
+export const HERO_TEXT = '...and I am your next marketing team member!';
+export const HERO_IMAGE_ALT = 'Veronica Mangrich headshot image';
