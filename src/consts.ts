75a0bd06e561940890547d158941170ae0d59479
Phil Boden <philip.boden@gmail.com>
2026-08-30 14:43:59 -0700

    meta information update

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Veronica Mangrich';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Veronica Mangrich | Portfolio';
+export const SITE_DESCRIPTION = 'Portfolio and resume website for Veronic Mangrich';
 
 export const HERO_TITLE = 'Hi, My name is Veronica Mangrich';
 export const HERO_TEXT = '...and I am your next marketing team member!';
